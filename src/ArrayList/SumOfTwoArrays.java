87044759efa76d616677fb55a0046ac4312a8b5d
techpractitioner <bhartiswami918@gmail.com>
2024-02-02 23:38:16 +0530

    Sum of two arrays problem statement

---
+++ src/ArrayList/SumOfTwoArrays.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+//input:
+//	one: [9, 9, 8, 7]
+//	two: [6, 5]
+//output: [1, 0, 0, 5, 2]
+//constaraints:
+//	size of arrays can be same
+//	return ArrayList
+//	You can't treat it at digits like 9987 + 65, treat these like arrays
+
+public class SumOfTwoArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
